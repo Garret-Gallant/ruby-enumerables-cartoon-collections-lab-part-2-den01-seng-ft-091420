e64097881ea17d2aca858b83fbe5804293f28e64
Garret Gallant <gallanttechnological@gmail.com>
2020-09-04 19:28:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,5 +29,6 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |str|
     str == valid_calls.includes "!"
+    end
   end
 end
